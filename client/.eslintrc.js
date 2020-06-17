d961d76fbd9fd26c7bf4a471ccc6c533013ae6d3
Veronika Melnikova <veronika.melnikova@non.se.com>
2020-06-17 13:32:52 +0400

    add eslint and prettier

---
+++ client/.eslintrc.js
@@ -0,0 +1,28 @@
+export default {
+    "env": {
+        "browser": true,
+        "es2020": true
+    },
+    "extends": [
+        "eslint:recommended",
+        "plugin:react/recommended",
+        "plugin:@typescript-eslint/eslint-recommended",
+        "plugin:@typescript-eslint/recommended",
+        "prettier"
+    ],
+    "parser": "@typescript-eslint/parser",
+    "parserOptions": {
+        "ecmaFeatures": {
+            "jsx": true
+        },
+        "ecmaVersion": 11,
+        "sourceType": "module"
+    },
+    "plugins": [
+        "react",
+        "@typescript-eslint"
+    ],
+    "rules": {
+        "prettier/prettier": "error"
+    }
+};
